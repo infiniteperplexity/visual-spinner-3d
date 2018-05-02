b7d212919f0fd75503da4ca64c379fb46b923ecd
infiniteperplexity <infinite.perplexity@gmail.com>
2018-05-01 23:09:32 -0700

    got started on React thing

--- react-vs3d.jsx
+++ react-vs3d.jsx
@@ -1,15 +1,59 @@
 let destination = document.querySelector("#container");
- 
+const UNIT = 50;
+const UNITS = 11;
+const HALF = UNIT/2;
+
 // A basic React component with some properties that I don't manually create
 class Grid extends React.Component {
   render() {
+    let grid = [];
+    for (let i=0; i<UNITS; i++) {
+      grid.push([])
+      for (let j=0; j<UNITS; j++) {
+        grid[i].push(<GridTarget key={i+","+j} x={i*UNIT+HALF} y={j*UNIT+HALF} />);
+      }
+    }
     return (
-      <div>
-        <p>Hello World!</p>
-      </div>
+      <svg width={UNIT*UNITS} height={UNIT*UNITS}>
+        {grid}
+        <Dragger x={HALF*UNITS} y={HALF*UNITS} />
+      </svg>
     );
   }
 };
+
+          // draggable="true"
+          // onDragStart={this.handleDrag}
+          // onDragOver={this.allowDrop}
+          // onDragLeave={this.dragLeave}
+          // onDrop={this.handleDrop}
+
+class GridTarget extends React.Component {
+  allowDrop = (event) => {
+    event.preventDefault();
+    event.target.style.strokeWidth = 3;
+  }
+  dragLeave = (event) => {
+    event.preventDefault();
+    event.target.style.strokeWidth = 1;
+  }
+  render() {
+    let {x, y} = this.props;
+    return [
+      <line key="h" x1={x-HALF} y1={y} x2={x+HALF} y2={y} style={{stroke: "gray", strokeWidth: 1}}
+        onDragOver={this.allowDrop} onDragLeave={this.dragLeave} />,
+      <line key="v" x1={x} y1={y-HALF} x2={x} y2={y+HALF} style={{stroke: "gray", strokeWidth: 1}} 
+        onDragOver={this.allowDrop} onDragLeave={this.dragLeave} />
+    ]
+  };
+}
+
+function Dragger({x, y}) {
+  return [
+    <circle draggable="true" key="0" cx={x} cy={y} r={3} stroke="black" strokeWidth="6" fill="black" />,
+    <circle draggable="true" key="1" cx={y} cy={y} r={2*UNIT} stroke="gray" strokeWidth="1" fill="none" />
+  ]
+}
 // A Higher-Order Component made using ReactRedux.connect
   // attaches properties to the "wrapped" component
 let App = ReactRedux.connect(
@@ -43,3 +87,10 @@ ReactDOM.render(
   </ReactRedux.Provider>,
   destination
 );
+
+/*
+Let's look at SVG.  It's what d3 uses.
+
+circle, rect
+
+*/
