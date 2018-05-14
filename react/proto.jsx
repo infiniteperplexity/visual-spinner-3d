68ee88619281f36f5010048f710c5a10498c1e9a
infiniteperplexity <Glenn.T.Wright@kp.org>
2018-05-14 11:47:08 -0700

    got the simplest react-redux case genuinely working

--- react/proto.jsx
+++ react/proto.jsx
@@ -1,27 +1,24 @@
-// http://www.codedread.com/blog/archives/2005/12/21/how-to-enable-dragging-in-svg/
-
-// might need a polyfill for Internet Explorer for getScreenCTM?
-
-// var m = El.getScreenCTM();
-// var p = document.documentElement.createSVGPoint();
-// p.x = evt.clientX;
-// p.y = evt.clientY;
-// p = p.matrixTransform(m.inverse());
-
-// and point p will now be in user coordinate system of the element El.
-
-//Returns a DOMMatrix representing the matrix that transforms the current element's coordinate system to the coordinate system of the SVG viewport for the SVG document fragment.
-
 let destination = document.querySelector("#container");
 const UNIT = 50;
 const UNITS = 11;
 const HALF = UNIT/2;
 
+let store;
 
+let propRegistry = {
+  "red": {
+    hand: {
+      x: 0,
+      y: 0
+    },
+    head: {
+      x: 1,
+      y: 1
+    }
+  }
+};
 
-// A basic React component with some properties that I don't manually create
-
-let DragSpaces = {}
+let DragSpaces = {};
 
 class DragSVG extends React.Component {
   constructor(props, context) {
@@ -68,47 +65,34 @@ class Grid extends React.Component {
     for (let i=0; i<UNITS; i++) {
       grid.push([])
       for (let j=0; j<UNITS; j++) {
-        grid[i].push(<GridTarget key={i+","+j} x={i*UNIT+HALF} y={j*UNIT+HALF} />);
+        grid[i].push(<GridTarget key={i+","+j}  x={i*UNIT+HALF} y={j*UNIT+HALF} />);
       }
     }
     let dragID = "WALL";
     return (
       <DragSVG dragID={dragID} width={UNIT*UNITS} height={UNIT*UNITS}>
         {grid} 
-        <Draggable dragID={dragID}>
+        <PropNodeComponent dragID={dragID} {...this.props}>
           <circle cx={HALF*UNITS} cy={HALF*UNITS} r={HALF} stroke="gray" strokeWidth="1" fill="green" />
-        </Draggable>
+        </PropNodeComponent>
       </DragSVG>
     );
   }
 };
-// <svg width={UNIT*UNITS} height={UNIT*UNITS}>
-// {grid}
-// <DragRect svg={this.svg} x={HALF*UNITS} y={HALF*UNITS} width={UNIT} height={UNIT} />
 
-
-class GridTarget extends React.Component {
-  render() {
-    let {x, y} = this.props;
-    return [
-      <line key="h" x1={x-HALF} y1={y} x2={x+HALF} y2={y} style={{stroke: "gray", strokeWidth: 1}} />,
-      <line key="v" x1={x} y1={y-HALF} x2={x} y2={y+HALF} style={{stroke: "gray", strokeWidth: 1}} />
-    ]
-  };
-}
-
-class Draggable extends React.Component {
+class PropNodeComponent extends React.Component {
   constructor(props, context) {
     super(props, context);
-    this.dragID = props.dragID;
-    this.state = {
+    // this stuff is not really "state" in the sense Redux cares about
+    this.info = {
+      dragID: props.dragID,
       beingDragged: false,
       xoffset: 0,
       yoffset: 0,
-      anchorX: 0,
-      anchorY: 0
-    };
-    this.svg = props.svg;
+      svg: props.svg,
+      point: null,
+      matrix: null
+    }
   }
   componentDidMount() {
     let e = this.element;
@@ -119,26 +103,24 @@ class Draggable extends React.Component {
       }
     }
     // matrix transformation stuff
-    this.point = e.createSVGPoint();
-    this.matrix = this.element.getScreenCTM().inverse();
+    this.info.point = e.createSVGPoint();
+    this.info.matrix = this.element.getScreenCTM().inverse();
   }
   handleMouseDown = (event) => {
     event.preventDefault();
-    this.setState({beingDragged: true});
-    DragSpaces[this.dragID].dragging = this;
+    this.info.beingDragged = true;
+    DragSpaces[this.info.dragID].dragging = this;
     // note: harmless violation of React state management practices
-    this.point.x = event.clientX;
-    this.point.y = event.clientY;
-    let p = this.point.matrixTransform(this.matrix);
-    this.setState({xoffset: p.x - this.state.anchorX});
-    this.setState({yoffset: p.y - this.state.anchorY});
-    console.log("offset:",this.state.xoffset, this.state.yoffset);
+    this.info.point.x = event.clientX;
+    this.info.point.y = event.clientY;
+    let p = this.info.point.matrixTransform(this.info.matrix);
+    this.info.xoffset = p.x - this.props.anchorX;
+    this.info.yoffset = p.y - this.props.anchorY;
   }
   handleMouseUp = (event) => {
     event.preventDefault();
-    this.setState({beingDragged: false});
-    DragSpaces[this.dragID].dragging = null;
-    console.log("anchor:",this.state.anchorX, this.state.anchorY);
+    this.info.beingDragged = false;
+    DragSpaces[this.info.dragID].dragging = null;
   }
   handleMouseLeave = (event) => {
     //event.preventDefault();
@@ -147,19 +129,19 @@ class Draggable extends React.Component {
   handleMouseMove = (event) => {
     event.preventDefault();
     // note: harmless violation of React state management practices
-    this.point.x = event.clientX;
-    this.point.y = event.clientY;
-    if(this.state.beingDragged) {
-      let p = this.point.matrixTransform(this.matrix);
-      this.setState({anchorX: p.x-this.state.xoffset});
-      this.setState({anchorY: p.y-this.state.yoffset});
+    this.info.point.x = event.clientX;
+    this.info.point.y = event.clientY;
+    if (this.info.beingDragged) {
+      let p = this.info.point.matrixTransform(this.info.matrix);
+      this.props.setX(p.x-this.info.xoffset);
+      this.props.setY(p.y-this.info.yoffset);
     }
   }
   render() {
     return (
       <g 
         ref={(e)=>(this.element=e)}
-        transform={"translate("+this.state.anchorX+","+this.state.anchorY+")"}
+        transform={"translate("+this.props.anchorX+","+this.props.anchorY+")"}
         onMouseDown={this.handleMouseDown}
         onMouseUp={this.handleMouseUp}
         onMouseMove={this.handleMouseMove}
@@ -174,39 +156,48 @@ class Draggable extends React.Component {
 // A Higher-Order Component made using ReactRedux.connect
   // attaches properties to the "wrapped" component
 let App = ReactRedux.connect(
-  (state)=>({}),
+  (state)=>({
+    anchorX: state.anchorX,
+    anchorY: state.anchorY
+  }),
   (dispatch)=>({
-      f: ()=>{dispatch({})}
+      setX: (x)=>dispatch({type: "setX", x: x}),
+      setY: (y)=>dispatch({type: "setY", y: y})
   })
 )(Grid);
 
 // a reducer function for a Redux store
 function reducer(state, action) {
   if (state === undefined) {
-    return {n: 0};
+    return {
+      anchorX: 0,
+      anchorY: 0
+    };
   }
-  let n = state.n;
   switch (action.type) {
-    case "inc":
-      return {n: n+1};
-    case "dec":
-      return {n: n-1};
+    case "setX":
+      return {...state, anchorX: action.x};
+    case "setY":
+      return {...state, anchorY: action.y};
     default:
       return state;
   }
 }
 
-// create the store inline
+class GridTarget extends React.Component {
+  render() {
+    let {x, y} = this.props;
+    return [
+      <line key="h" x1={x-HALF} y1={y} x2={x+HALF} y2={y} style={{stroke: "gray", strokeWidth: 1}} />,
+      <line key="v" x1={x} y1={y-HALF} x2={x} y2={y+HALF} style={{stroke: "gray", strokeWidth: 1}} />
+    ]
+  };
+}
+
+store = Redux.createStore(reducer);
 ReactDOM.render(
-  <ReactRedux.Provider store={Redux.createStore(reducer)}>
+  <ReactRedux.Provider store={store}>
     <App />
   </ReactRedux.Provider>,
   destination
 );
-
-/*
-Let's look at SVG.  It's what d3 uses.
-
-circle, rect
-
-*/
