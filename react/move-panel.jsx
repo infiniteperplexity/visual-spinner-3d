160d686e0ffc0a13ac4d103c7d2ea80910f64278
Glenn T Wright <Glenn.T.Wright@kp.org>
2018-06-22 12:33:52 -0700

    move panel is starting to look a lot better

--- react/move-panel.jsx
+++ react/move-panel.jsx
@@ -143,28 +143,97 @@ class MovePanel extends React.Component {
     const TEXT = 35;
     return (
       <div className="grid movepanel"> 
-        <svg height={12*SVG} width={2*SVG} onMouseDown={()=>{}}>
-          <HeadNode x={SVG} y={1.3*SVG} dim={SVG/4} fill={color} />
-          <g onMouseDown={this.setGripLock}>
-            <GripNode x={SVG} y={3.4*SVG} dim={SVG/8} fill={gcolor}/>
-          </g>
-          <HandNode x={SVG} y={5.4*SVG} dim={SVG/5} fill={color}/>
-          <g onMouseDown={this.setHelperLock}>
-            <HelperNode x={SVG} y={7.5*SVG} dim={SVG/5} fill={hcolor} onMouseDown={this.setHelperLock}/>
-          </g>
-          <PivotNode x={SVG} y={9.5*SVG} dim={SVG/5} fill={color}/>
-          <g onMouseDown={this.setBodyLock}>
-            <BodyNode x={SVG} y={11.5*SVG} dim={UNIT/6} fill={bcolor} onMouseDown={this.setBodyLock}/>
+        <MoveControl node="head" {...this.props}/>
+        <MoveControl node="grip" {...this.props}/>
+        <MoveControl node="hand" {...this.props}/>
+        <MoveControl node="helper" {...this.props}/>
+        <MoveControl node="pivot" {...this.props}/>
+        <MoveControl node="body" {...this.props}/>
+      </div>
+    );
+  }
+}
+
+class MoveControl extends React.Component {
+  handleMouseDown = (e)=>{
+    if (this.props.frozen) {
+      return;
+    }
+    if (!["head","grip","pivot","helper"].includes(this.props.node)) {
+      return;
+    }
+    let node = this.props.node;
+    let locked = this.props.locks[node];
+    this.props.setLock(node, !locked);
+    this.props.checkLocks();
+  }
+  render() {
+    const SVG = 25;
+    let graphic;
+    let propid = this.props.order[this.props.order.length-1];
+    let node = this.props.node;
+    let color = this.props.colors[propid];
+    // !!!! needs a visual cue for tether lock
+    let locked = (["grip","helper","body"].includes(node) && this.props.locks[node]);
+    if (locked) {
+      color = "lightgray";
+    }
+    if (node==="body") {
+      graphic = <BodyNode x={0.5*SVG} y={SVG} dim={SVG/6} fill={color}/>;
+    } else if (node==="pivot") {
+      graphic = <PivotNode x={0.5*SVG} y={SVG} dim={SVG/5} fill={color}/>;
+    } else if (node==="helper") {
+      graphic = <HelperNode x={0.5*SVG} y={SVG} dim={SVG/5} fill={color}/>;
+    } else if (node==="hand") {
+      if (this.props.locks.grip) {
+        graphic = <GripNode x={0.5*SVG} y={SVG} dim={SVG/9} fill={color}/>;
+      } else {
+        graphic = <HandNode x={0.5*SVG} y={SVG} dim={SVG/5} fill={color}/>;
+      }
+    } else if (node==="grip") {
+      graphic = <GripNode x={0.5*SVG} y={SVG} dim={SVG/9} fill={color}/>;
+    } else if (node==="head") {
+      graphic = <HeadNode x={0.5*SVG} y={SVG} dim={SVG/5} fill={color}/>;
+    }
+    let buttons = (locked) ? null : [
+        <button style={{height: "52px"}} key="0">Slows Down More</button>,
+        <button key="1">More Counterclockwise</button>,
+        <button key="2">More Clockwise</button>,
+        <button key="3">Speeds Up More</button>
+    ];
+    let tether1 = {stroke: "gray"};
+    let tether2 = {stroke: "gray"};
+    if (node==="head") {
+      tether1.strokeWidth = 3;
+      if (!this.props.locks.head) {
+        tether1.strokeDasharray = "3,3";
+      }
+      tether2.strokeWidth = 0;
+    } else if (node==="grip") {
+      tether1.strokeDasharray = "5,5";
+      if (!this.props.locks.head) {
+        tether2.strokeDasharray = "3,3";
+      }
+      tether2.strokeWidth = 3;
+    } else if (node==="body") {
+      tether1.strokeWidth = 0;
+      tether2.strokeDasharray="5,5";
+    } else {
+      tether1.strokeDasharray="5,5";
+      tether2.strokeDasharray="5,5";
+    }
+    return (
+      <div className="grid movecontrol">
+        <svg height={2*SVG} width={SVG}>
+          <g onMouseDown={this.handleMouseDown}>
+            <line x1={0.5*SVG} y1={0} x2={0.5*SVG} y2={SVG} style={tether2} />
+            <line x1={0.5*SVG} y1={SVG} x2={0.5*SVG} y2={2*SVG} style={tether1} />
+            {graphic}
+            
           </g>
         </svg>
-        <div style={{lineHeight: TEXT+"px"}}>
-          <p>head</p>
-          <p>grip</p>
-          <p>hand</p>
-          <p>helper</p>
-          <p>pivot</p>
-          <p>body</p>
-        </div>
+        <p style={{color: (locked) ? "lightgray" : "black"}}>{node}</p>
+        {buttons}
       </div>
     );
   }
