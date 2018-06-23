5e5e36ac916b36fcd6bf62152cd5029817696903
infiniteperplexity <infinite.perplexity@gmail.com>
2018-06-22 22:43:44 -0700

    continuing work on the move panel

--- react/move-panel.jsx
+++ react/move-panel.jsx
@@ -1,108 +1,3 @@
-class NumberPanel extends React.Component {
-  handleChange = (e)=>{
-    if (this.props.frozen) {
-      return;
-    }
-    // !!!might need to change state.locks
-    // this.props.modifyMove({...this.prop.vals, value: e.target.value});
-    let nodes = {};
-    let node = {};
-    node[this.props.vals.moment] = parseFloat(e.target.value);
-    nodes[NODES[this.props.vals.node]] = node;
-    this.props.modifyMove({
-      propid: this.props.vals.propid,
-      tick: this.props.tick,
-      nodes: nodes
-    });
-    this.props.resolveMove({
-      propid: this.props.vals.propid,
-      tick: this.props.tick
-    });
-    this.props.pushState();
-    this.props.renderEngine();
-  }
-  render() {
-    return <input type="number" onChange={this.handleChange} style={{width: 50, fontFamily: "monospace"}} value={this.props.value} />
-  }
-}
-
-class LockBox extends React.Component {
-  handleChange = (e) => {
-    if (this.props.frozen) {
-      return;
-    }
-    this.props.setLock(this.props.node, e.target.checked);
-    // !!!!This is one possibility; another is to overwrite.
-    this.props.checkLocks();
-  }
-  render() {
-    let bool = clone(this.props.locks)[this.props.node];
-    return (
-      <span>
-        <input onChange={this.handleChange} type="checkbox" value="someDamnthing" checked={bool}/>{this.props.children}
-      </span>
-    );
-  }
-}
-
-class MovePanel1 extends React.Component {
-  // the last item on order is the active one
-  render() {
-    let propid = this.props.order[this.props.order.length-1];
-    let move;
-    if (this.props.tick===-1) {
-      move = this.props.starters[propid];
-    } else if (this.props.moves[propid].length===0) {
-      return null;
-    } else {
-      move = submove(this.props.moves[propid], this.props.tick).move;
-    }
-    let list = [];
-    for (let i=NODES.length-1; i>=0; i--) {
-      let node = NODES[i];
-      let spacer = (node==="pivot") ? <span>&nbsp;</span> : <span>&nbsp;&nbsp;</span>;
-      let color = "black";
-      if (["grip","helper","body"].includes(node) && this.props.locks[node]) {
-        color = "gray";
-      }
-      if (this.props.tick===-1) {
-        color = "gray";
-      }
-      list.push(
-        <div key={i} style={{color: color}}>
-          {(node==="helper") ? "help" : node}{spacer}v<sub>angle0</sub>&nbsp;<NumberPanel vals={{propid: propid, node: i, moment: "va"}} value={move[node].va} {...this.props}/>
-          &nbsp;v<sub>angle1</sub>&nbsp;<NumberPanel vals={{propid: propid, node: i, moment: "va1"}} value={move[node].va1} {...this.props}/>
-          {(["grip","helper","body"].includes(node)) ? <LockBox node={node} {...this.props}>lock {node}</LockBox>: null}
-          <br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;v<sub>radius0</sub><NumberPanel vals={{propid: propid, node: i, moment: "vr"}} value={move[node].vr} {...this.props}/>
-          &nbsp;v<sub>radius1</sub><NumberPanel vals={{propid: propid, node: i, moment: "vr1"}} value={move[node].vr1} {...this.props}/>
-          {(node==="head") ? <LockBox node={node} {...this.props}>lock tether</LockBox>: null}
-        </div>
-      );
-    }
-    return (
-      <div style={{fontFamily: "monospace"}}>
-        @tick  <input type="text" size="5" readOnly value={this.props.tick} />
-        &nbsp;ticks <NumberPanel  vals={{propid: propid, node: null, moment: "ticks"}} step={90} value={move.beats*BEAT} {...this.props}/>
-          {list}
-          <div>
-            plane <input type="text" size="5" readOnly value={this.props.plane} />
-            <br />
-            bend<sub>0</sub>&nbsp;
-              <NumberPanel vals={{propid: propid, node: null, moment: "bent"}} value={move.bent} {...this.props} />
-            &nbsp;v<sub>bend</sub>&nbsp;
-              <NumberPanel vals={{propid: propid, node: null, moment: "vb"}} value={move.vb} {...this.props} />
-            &nbsp;twist<sub>0</sub>&nbsp;
-            <NumberPanel vals={{propid: propid, node: null, moment: "twist"}} value={move.twist} {...this.props}/>
-            &nbsp;v<sub>twist</sub>&nbsp;
-              <NumberPanel vals={{propid: propid, node: null, moment: "vt"}} value={move.vt} {...this.props}/>
-          </div>
-        </div>
-    )
-  }
-}
-
-
 class MovePanel extends React.Component {
   // the last item on order is the active one
   setLock = (e, node)=>{
@@ -146,7 +41,7 @@ class MovePanel extends React.Component {
         <span>
           {"duration "+"\u231B"}
         </span>
-        <input type="number" style={{width: "72px"}} value={beats(move)*BEAT}/>
+        {beats(move)*BEAT}
       </div>
     ;
     if (this.props.tick===-1) {
@@ -216,20 +111,17 @@ class MoveControl extends React.Component {
     } else if (node==="head") {
       graphic = <HeadNode x={SVG} y={SVG} dim={SVG/5} fill={color}/>;
     }
-    let font = {fontSize: "32px", fontWeight: "bold", width: "50px", color: color};
-    console.log(submove);
     let move;
     if (this.props.tick>-1) {
       move = submove(this.props.moves[propid], this.props.tick).move;
     }
+    let button = {paddingBottom: "28px", paddingRight: "25px", height: "22px", width: "22px", fontSize: "18px", fontWeight: "bold", color: color};
     let buttons = (locked || this.props.tick===-1) ? null : [
-        <button key="0" title="More Counterclockwise / Less Clockwise Spin" style={font}>{"\u21BA"}</button>,
-        <button key="1" title="More Clockwise / Less Counterclockwise Spin" style={font}>{"\u21BB"}</button>,
-        <p key="2" title="1 Clockwise Rotation, Constant Speed" type="text">{
-          ((move[node]) ? move[node].va : 0)+"\u21BB"+"\u21D2"+((move[node]) ? move[node].va1 : 0)+"\u21BB"
-        }</p>,
-        <button key="3" title="Starts Slower, Ends Faster" style={font}>{"\u219E"}</button>,
-        <button key="4" title="Starts Faster, Ends Slower" style={font}>{"\u21A0"}</button>
+        <SpeedMeter key="0" move={move} color={color} node={node}/>,
+        <button key="1" title="More Counterclockwise / Less Clockwise Spin" style={button}>{"\u21BA"}</button>,
+        <button key="2" title="More Clockwise / Less Counterclockwise Spin" style={button}>{"\u21BB"}</button>,
+        <button key="3" title="Starts Slower, Ends Faster" style={button}>{"\u219E"}</button>,
+        <button key="4" title="Starts Faster, Ends Slower" style={button}>{"\u21A0"}</button>
     ];
     let tether1 = {stroke: "gray"};
     let tether2 = {stroke: "gray"};
@@ -252,12 +144,27 @@ class MoveControl extends React.Component {
       tether1.strokeDasharray="5,5";
       tether2.strokeDasharray="5,5";
     }
-    let title = (this.props.locks[node]===undefined) ? null : <title>click me.</title>;
+    let title;
+    if (node==="head" && this.props.locks.head) {
+      title = "Click to 'unlock' the tether, allowing its length to change.";
+    } else if (node==="head") {
+      title = "Click to 'lock' the tether, preventing its length from changing.";
+    } else if (node==="hand") {
+      title = "This node represents the spinner's hand.";
+    } else if (node==="grip") {
+      title = "Click to "+((locked)?"en":"dis")+"able the 'handle' node, which represents varying where hand grips the prop.";
+    } else if (node==="helper") {
+      title = "Click to "+((locked)?"en":"dis")+"able the 'elbow' node, which is sometimes used to fine-tune movements.";
+    } else if (node==="pivot") {
+      title = "The 'shoulder' node is often used to shift moves horizontally or vertically.";
+    } else if (node==="body") {
+      title = "Click to "+((locked)?"en":"dis")+"able the 'body' node, which represents the spinner's body moving.";
+    }
     return (
       <div className="grid movecontrol">
         <svg height={2*SVG} width={2*SVG}>
           <g onMouseDown={this.handleMouseDown}>
-            {title}
+            <title>{title}</title>
             <line x1={SVG} y1={0} x2={SVG} y2={SVG} style={tether2} />
             <line x1={SVG} y1={SVG} x2={SVG} y2={2*SVG} style={tether1} />
             {graphic}
@@ -270,3 +177,43 @@ class MoveControl extends React.Component {
   }
 }
 
+function SpeedMeter(props, context) {
+  let {move, node, color} = props;
+  let va = move[node] ? move[node].va : 0;
+  let va1 = move[node] ? move[node].va1 : 0;
+  let spin = beats(move)/4;
+  let spins = Math.ceil(Math.abs(va*spin));
+  let spins1 = Math.ceil(Math.abs(va1*spin));
+  let sym0 = <text textAnchor="middle" x={11} y={35} fill={color} style={{fontSize: "32px", fontWeight: "bold"}}>{(va<0) ? "\u21BA" : "\u21BB"}</text>;
+  let sym1 = <text textAnchor="middle" x={41} y={35} fill={color} style={{fontSize: "32px", fontWeight: "bold"}}>{(va1<0) ? "\u21BA" : "\u21BB"}</text>;
+  let text1 = <text textAnchor="middle" x={(va<0) ? 11 : 12} y={31} style={{fontSize: "12px"}}>{spins}</text>;
+  let text2 = <text textAnchor="middle" x={(va<0) ? 26 : 27} y={31} style={{fontSize: "12px"}}>{"\u2B62"}</text>;
+  let text3 = <text textAnchor="middle" x={(va1<0) ? 41 : 42} y={31} style={{fontSize: "12px"}}>{spins1}</text>;
+  let niceNames = {
+    body: "body",
+    pivot: "shoulder",
+    helper: "elbow",
+    hand: "hand",
+    grip: "handle",
+    head: "head"
+  };
+  let title = niceNames[node] + " starts at "+va+" spins, ends at "+va1+" spins per measure";
+  if (nearly(va, va1)) {
+    sym0 = <text textAnchor="middle" x={26} y={35} fill={color} style={{fontSize: "32px", fontWeight: "bold"}}>{(va<0) ? "\u21BA" : "\u21BB"}</text>;
+    text1 = <text textAnchor="middle" x={(va<0) ? 26 : 27} y={31} style={{fontSize: "12px"}}>{spins}</text>;
+    sym1 = null;
+    text2 = null;
+    text3 = null;
+    title = niceNames[node]+" speed is "+va+" spins per measure";
+  }
+  return (
+    <svg height={49} width={80}>
+      <title>{title}</title>
+      {sym0}
+      {sym1}
+      {text1}
+      {text2}
+      {text3}
+    </svg>
+  );
+}
